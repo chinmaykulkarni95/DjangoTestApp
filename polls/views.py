2f058495d981bf08888bd143544a2cc3141c9a85
chinmaykulkarni95 <kulkarni.chinmayrules@gmail.com>
2022-02-19 01:06:32 -0500

    Added Views

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import render
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
